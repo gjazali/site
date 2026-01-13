d18e4d9ac96de0f52b18e5291227b0d6791f42b0
G.A. Jazali <dev@jazali.org>
2026-01-13 07:32:40 +0700

    chore: rename `/templates/blog.html` to `/templates/blog_post.html`

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,4 @@
 import { generatePosts } from "./generate_posts.js";
 
 console.log(`Generating blog posts...`);
-generatePosts("content/blog", "public/blog", "templates/blog.html");
+generatePosts("content/blog", "public/blog", "templates/blog_post.html");
