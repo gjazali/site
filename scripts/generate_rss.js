8ebc5b6d132c7c6f4e9cdc26c936f13fad6d09db
G.A. Jazali <dev@jazali.org>
2026-01-14 12:01:58 +0700

    feat(RSS): support `category`

--- scripts/generate_rss.js
+++ scripts/generate_rss.js
@@ -2,7 +2,8 @@ import fs from "fs";
 import path from "path";
 import { parseMetadataDir, metadataDescendingSorter } from "./utils.js";
 
-const rssItemTemplate = `<item><title>{{ RSS_ITEM_TITLE }}</title><link>{{ RSS_ITEM_LINK }}</link><description>{{ RSS_ITEM_DESCRIPTION }}</description><pubDate>{{ RSS_ITEM_PUBLICATION_DATE }}</pubDate><language>{{ RSS_ITEM_LANGUAGE }}</language></item>`;
+const rssItemTemplate = `<item><title>{{ RSS_ITEM_TITLE }}</title><link>{{ RSS_ITEM_LINK }}</link><description>{{ RSS_ITEM_DESCRIPTION }}</description><pubDate>{{ RSS_ITEM_PUBLICATION_DATE }}</pubDate><language>{{ RSS_ITEM_LANGUAGE }}</language>{{ RSS_ITEM_CATEGORIES }}</item>`;
+const rssItemCategoryTemplate = `<category>{{ RSS_ITEM_CATEGORY }}</category>`;
 
 export function generateRSS(inputDirPath, outputDirPath, templateFilePath, rssFileName, feedSourceDomain, feedSourceDir) {
   let items = [];
@@ -13,12 +14,22 @@ export function generateRSS(inputDirPath, outputDirPath, templateFilePath, rssFi
   fs.mkdirSync(outputDir, { recursive: true });
 
   for (const metadata of metadataList) {
+    let categories = [];
+
+    for (const category of metadata.category.sort()) {
+      categories.push(
+        rssItemCategoryTemplate
+          .replace("{{ RSS_ITEM_CATEGORY }}", category)
+      );
+    }
+
     const item = rssItemTemplate
       .replace("{{ RSS_ITEM_TITLE }}", metadata.title)
       .replace("{{ RSS_ITEM_LINK }}", `https://${feedSourceDomain}/${feedSourceDir}/${metadata.path}.html`)
       .replace("{{ RSS_ITEM_DESCRIPTION }}", metadata.description) // To ensure that the date format conforms with RFC 822
       .replace("{{ RSS_ITEM_PUBLICATION_DATE }}", new Date(metadata.created).toUTCString())
-      .replace("{{ RSS_ITEM_LANGUAGE }}", metadata.language);
+      .replace("{{ RSS_ITEM_LANGUAGE }}", metadata.language)
+      .replace("{{ RSS_ITEM_CATEGORIES }}", categories.join(""));
 
     items.push(item);
   }
