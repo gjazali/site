7fc3010f0cd141efc20638cdf40e19b2b1bca372
G.A. Jazali <dev@jazali.org>
2026-08-26 17:51:27 +0700

    fix: syntax bug with `new` & hardcoded hostname

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     if (url.pathname.startsWith("/feed") || url.pathname.startsWith("/rss")) {
-      return new Response.redirect("https://jazali.org/blog/index.xml", 307);
+      return Response.redirect(`${url.origin}/blog/index.xml`, 307);
     }
 
     return env.ASSETS.fetch(request);
